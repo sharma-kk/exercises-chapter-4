83af4a5055c686691e9d3d6d9b7ace984477a85d
sharma-kk <sharma.uhh@gmail.com>
2023-03-31 18:04:08 +0200

    solved exercises for chapter 4

--- life/life.py
+++ life/life.py
@@ -1,13 +1,10 @@
-import numpy as np
+import numpy as np  # noqa F401
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
-blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]])
+blinker = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -48,30 +45,77 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    """Conway's game of life implementation."""
+
+    def __init__(self, size):
+        """Initialize the game."""
+        self.board = np.zeros((size, size))
+
     def play(self):
+        """Play the game."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        """Convolution checks which value to assign at the next step."""
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbour_count = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                if (neighbour_count[i, j] == 3
+                    or (self.board and neighbour_count[i, j] == 2)):
+                    self.board[i, j] = 1
+                else:
+                    self.board[i, j] = 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """Set value at a perticular box on the board."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Show the next step."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
+
+    def insert(self, pat, ip):
+        """Insert a pattern, pat into the game board at some location, ip."""
+        self.board[ip[0]-1:ip[0]+2, ip[1]-1:ip[1]+2] = pat.grid
+
+
+class Pattern:
+    """Do things with pattern."""
+
+    def __init__(self, design):
+        """Initialize the class and take a numpy array as input."""
+        self.grid = design
+
+    def flip_vertical(self):
+        """Flip the pattern upside down."""
+        return Pattern(self.grid[::-1])
+
+    def flip_horizontal(self):
+        """Pattern is reversed left-right."""
+        return Pattern(np.flip(self.grid, 1))
+
+    def flip_diag(self):
+        """Transpose."""
+        return Pattern(np.transpose(self.grid))
+
+    def rotate(self, n):
+        """Rotate by n right angles anticlockwise."""
+        # a = self.grid
+        # for i in range(n):
+        #     a = np.transpose(a)[::-1]
+        # return Pattern(a)
+        a = self
+        for i in range(n):
+            a = a.flip_diag().flip_vertical()
+        return a
